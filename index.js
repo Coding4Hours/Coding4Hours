c28e3f0bbb19d0426e94122b4897e7e6b69aae0c
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-06 11:12:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear) * 100.toFixed(2)
 const progressBarOfThisYear = generateProgressBar()
 
 function generateProgressBar() {
@@ -14,9 +14,8 @@ function generateProgressBar() {
       '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
     return `{ ${progressBar} }`
 }
-
 const readmeContent = fs.readFileSync(`./README.md`, "utf-8").split("\n")
-readmeContent[1] = readmeContent[1] = `  ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100.toFixed(2))}%`)
+readmeContent[1] = readmeContent[1] = `  ⏳ Year progress ${progressBarOfThisYear} ${progressOfThisYear}%`)
 const readme = readmeContent.join("\n")
 
 fs.writeFile('./README.md', readme, function (err) {
