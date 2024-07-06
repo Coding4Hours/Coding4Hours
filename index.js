c9be709fcac563beb319b0701b1bfa9aef01282d
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-06 11:13:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear) * 100.toFixed(2)
+const progressOfThisYear = ((Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear) * 100).toFixed(2)
 const progressBarOfThisYear = generateProgressBar()
 
 function generateProgressBar() {
