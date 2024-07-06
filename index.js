188dcf3ea34821684239d9cb7d11be3ea1ed1796
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-06 10:02:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ function generateProgressBar() {
 }
 
 const readmeContent = fs.readFileSync(`./README.md`, "utf-8").split("\n")
-console.log(fs.readFileSync(`./README.md`, "utf-8"))
-console.log(readmeContent[1])
 readmeContent[1] = readmeContent[1].replace("date start", `⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %`)
 const readme = readmeContent.join("\n")
-console.log(readme)
+
+fs.writeFile('./README.MD', readme, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
