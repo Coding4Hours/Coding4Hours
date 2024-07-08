234c503a4a6d3386a94822b4c38dd338b3bc8a7d
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-08 15:37:58 -0700

    Create cloud_types.rb

---
+++ wordcloud/cloud_types.rb
@@ -0,0 +1,20 @@
+module CloudTypes
+  CLOUDLABELS = [
+    "quarantine",
+    "food",
+    "company",
+    "podcast",
+    "2021",
+    "travel",
+  ]
+
+  CLOUDPROMPTS = [
+    "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
+    
+    "Favourite Food? :yum: :pizza: :cookie:",
+    "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
+    "What podcast are you currently listening to? :microphone: :headphones: :notes:",
+    "What are you looking forward to in 2021? :tada: :sunglasses: :confetti_ball:",
+    "Where are you hoping to travel next? :airplane: :earth_americas: :luggage:"
+  ]
+end
