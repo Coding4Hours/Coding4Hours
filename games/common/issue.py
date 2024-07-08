0e6a28b556e11108f622cb18f4a914ce1a803516
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-08 10:05:10 -0700

    Update issue.py

--- games/common/issue.py
+++ games/common/issue.py
@@ -2,7 +2,7 @@ from github import Github
 from os import environ
 
 def GetInfo():
-    repo = Github(environ['GITHUB_TOKEN']).get_repo(environ['GITHUB_REPOSITORY'])
+    repo = Github(environ['GOD']).get_repo(environ['GITHUB_REPOSITORY'])
     issue = repo.get_issue(number=int(environ['ISSUE_NUMBER']))
 
     return issue, issue.user.login
