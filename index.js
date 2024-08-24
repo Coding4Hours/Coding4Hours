e998f3f8934065b2085911f52981bba0df8412bc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-08-24 10:02:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function generateProgressBar() {
     const passedProgressBarIndex = Math.max(0, Math.min(parseInt(progressOfThisYear * progressBarCapacity / 100), progressBarCapacity));
     const progressBar =
         '█'.repeat(passedProgressBarIndex) + '▁'.repeat(progressBarCapacity - passedProgressBarIndex);
-    return "{"  + progressBar "}";
+    return progressBar;
 }
 
 const readmeContent = fs.readFileSync(`./README.md`, "utf-8").split("\n");
-readmeContent[2] = `<p align="center"> <a href="https://octoprofile.vercel.app/user?id=Coding4Hours"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&vCenter=true&center=true&width=435&lines=%F0%9D%99%B7%F0%9D%9A%92%2C+%F0%9D%99%B8'%F0%9D%9A%96+%F0%9D%99%B2%F0%9D%9A%98%F0%9D%9A%8D%F0%9D%9A%92%F0%9D%9A%97%F0%9D%9A%90%F0%9D%9F%BA%F0%9D%99%B7%F0%9D%9A%98%F0%9D%9A%9E%F0%9D%9A%9B%F0%9D%9A%9C%F0%9F%91%8B;%F0%9D%99%B8'%F0%9D%9A%96+%F0%9D%9A%8A+%F0%9D%9A%A0%F0%9D%9A%8A%F0%9D%9A%97%F0%9D%9A%97%F0%9D%9A%8A%F0%9D%9A%8B%F0%9D%9A%8E+%F0%9D%9A%91%F0%9D%9A%8A%F0%9D%9A%8C%F0%9D%9A%94%F0%9D%9A%8E%F0%9D%9A%9B+%F0%9D%9A%8A%F0%9D%9A%97%F0%9D%9A%8D+%F0%9D%9A%8D%F0%9D%9A%8E%F0%9D%9A%9F%F0%9D%9A%8E%F0%9D%9A%95%F0%9D%9A%98%F0%9D%9A%99%F0%9D%9A%8E%F0%9D%9A%9B.;%F0%9D%99%B8+%F0%9D%9A%95%F0%9D%9A%98%F0%9D%9A%9F%F0%9D%9A%8E+%F0%9D%99%B8%F0%9D%9A%83.;%F0%9D%99%BF%F0%9D%9A%9B%F0%9D%9A%8E%F0%9D%9A%9C%F0%9D%9A%9C+%F0%9D%9A%8F%F0%9D%9A%98%F0%9D%9A%9B+%F0%9D%9A%96%F0%9D%9A%98%F0%9D%9A%9B%F0%9D%9A%8E+%F0%9D%9A%92%F0%9D%9A%97%F0%9D%9A%8F%F0%9D%9A%98!;%E2%8F%B3+%F0%9D%9A%88%F0%9D%9A%8E%F0%9D%9A%8A%F0%9D%9A%9B+%F0%9D%9A%99%F0%9D%9A%9B%F0%9D%9A%98%F0%9D%9A%90%F0%9D%9A%9B%F0%9D%9A%8E%F0%9D%9A%9C%F0%9D%9A%9C+${encodeURIComponent(progressBarOfThisYear)}+${encodeURIComponent(progressOfThisYear)}%25" alt="Typing SVG" /></a> </p>`;
+readmeContent[2] = `<p align="center"> <a href="https://octoprofile.vercel.app/user?id=Coding4Hours"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&vCenter=true&center=true&width=435&lines=𝙷𝚒,+𝙸'𝚖+𝙲𝚘𝚍𝚒𝚗𝚐𝟺𝙷𝚘𝚞𝚛𝚜👋;𝙸'𝚖+𝚊+𝚠𝚊𝚗𝚗𝚊𝚋𝚎+𝚑𝚊𝚌𝚔𝚎𝚛+𝚊𝚗𝚍+𝚍𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛.;𝙸+𝚕𝚘𝚟𝚎+𝙸𝚃.;𝙿𝚛𝚎𝚜𝚜+𝚏𝚘𝚛+𝚖𝚘𝚛𝚎+𝚒𝚗𝚏𝚘!;⏳+𝚈𝚎𝚊𝚛+𝚙𝚛𝚘𝚐𝚛𝚎𝚜𝚜+{+${progressBarOfThisYear}+}+52.72%" alt="Typing SVG" /></a> </p>`;
 const readme = readmeContent.join("\n");
 
 fs.writeFile('./README.md', readme, function (err) {
