7ac5ffb6bc98955d722e1c248ffd2739fea24cc7
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-05-16 18:42:13 -0700

    Delete games directory

--- games/common/issue.py
+++
@@ -1,8 +0,0 @@
-from github import Github
-from os import environ
-
-def GetInfo():
-    repo = Github(environ['GOD']).get_repo(environ['GITHUB_REPOSITORY'])
-    issue = repo.get_issue(number=int(environ['ISSUE_NUMBER']))
-
-    return issue, issue.user.login
